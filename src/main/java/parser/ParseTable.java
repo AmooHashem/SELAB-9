d9e2399f6252204458239b4dc49be33c67291edc
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2022-06-24 18:14:19 +0430

    Split Method Into Smaller Ones

--- src/main/java/parser/ParseTable.java
+++ src/main/java/parser/ParseTable.java
@@ -9,60 +9,50 @@ import java.util.*;
  * Created by mohammad hosein on 6/25/2015.
  */
 public class ParseTable {
-    private ArrayList<Map<Token,Action>> actionTable;
-    private ArrayList<Map<NonTerminal,Integer>> gotoTable;
+    private final ArrayList<Map<Token, Action>> actionTable = new ArrayList<>();
+    private final ArrayList<Map<NonTerminal, Integer>> gotoTable = new ArrayList<>();
+
     public ParseTable(String jsonTable) throws Exception {
-        jsonTable = jsonTable.substring(2,jsonTable.length()-2);
-        String[] Rows = jsonTable.split("\\],\\[");
-        Map<Integer, Token> terminals = new HashMap<Integer, Token>();
-        Map<Integer,NonTerminal> nonTerminals = new HashMap<Integer, NonTerminal>();
-        Rows[0] = Rows[0].substring(1,Rows[0].length()-1);
-        String[] cols = Rows[0].split("\",\"");
+        jsonTable = jsonTable.substring(2, jsonTable.length() - 2);
+        String[] rows = jsonTable.split("],\\[");
+        Map<Integer, Token> terminals = new HashMap<>();
+        Map<Integer, NonTerminal> nonTerminals = new HashMap<>();
+        rows[0] = rows[0].substring(1, rows[0].length() - 1);
+        calcTokens(rows[0].split("\",\""), terminals, nonTerminals);
+        calcTables(rows, terminals, nonTerminals);
+    }
+
+    private void calcTokens(String[] cols, Map<Integer, Token> terminals, Map<Integer, NonTerminal> nonTerminals) {
         for (int i = 1; i < cols.length; i++) {
-            if(cols[i].startsWith("Goto")){
+            if (cols[i].startsWith("Goto")) {
                 String temp = cols[i].substring(5);
                 try {
                     nonTerminals.put(i, NonTerminal.valueOf(temp));
-                }catch (Exception e){
-                    temp = temp;
+                } catch (Exception ignored) {
                 }
-            }
-            else {
+            } else {
                 terminals.put(i, new Token(Token.getTyepFormString(cols[i]), cols[i]));
             }
         }
-        actionTable = new ArrayList<Map<Token, Action>>();
-        gotoTable = new ArrayList<Map<NonTerminal, Integer>>();
-        for (int i = 1; i <Rows.length ; i++) {
-            if(i == 100)
-            {
-                int a= 1;
-                a++;
-            }
-            Rows[i] = Rows[i].substring(1,Rows[i].length()-1);
-            cols = Rows[i].split("\",\"");
+    }
+
+    private void calcTables(String[] rows, Map<Integer, Token> terminals, Map<Integer, NonTerminal> nonTerminals) throws Exception {
+        for (int i = 1; i < rows.length; i++) {
+            rows[i] = rows[i].substring(1, rows[i].length() - 1);
+            String[] cols = rows[i].split("\",\"");
             actionTable.add(new HashMap<Token, Action>());
             gotoTable.add(new HashMap<>());
-            for (int j = 1; j <cols.length ; j++) {
-                if(!cols[j].equals(""))
-                {
-                    if(cols[j].equals("acc")){
-                        actionTable.get(actionTable.size()-1).put(terminals.get(j),new Action(act.accept,0));
-                    }else
-                    if(terminals.containsKey(j))
-                    {
-//                        try {
+            for (int j = 1; j < cols.length; j++) {
+                if (!cols[j].equals("")) {
+                    if (cols[j].equals("acc")) {
+                        actionTable.get(actionTable.size() - 1).put(terminals.get(j), new Action(act.accept, 0));
+                    } else if (terminals.containsKey(j)) {
                         Token t = terminals.get(j);
                         Action a = new Action(cols[j].charAt(0) == 'r' ? act.reduce : act.shift, Integer.parseInt(cols[j].substring(1)));
-                            actionTable.get(actionTable.size() - 1).put(t, a);
-//                        }catch (StringIndexOutOfBoundsException e){
-//                            e.printStackTrace();
-//                        }
-                    }
-                    else if(nonTerminals.containsKey(j)){
-                        gotoTable.get(gotoTable.size()-1).put(nonTerminals.get(j),Integer.parseInt(cols[j]));
-                    }
-                    else {
+                        actionTable.get(actionTable.size() - 1).put(t, a);
+                    } else if (nonTerminals.containsKey(j)) {
+                        gotoTable.get(gotoTable.size() - 1).put(nonTerminals.get(j), Integer.parseInt(cols[j]));
+                    } else {
                         throw new Exception();
                     }
                 }
@@ -70,19 +60,13 @@ public class ParseTable {
         }
     }
 
-    public int getGotoTable(int currentState, NonTerminal variable )
-    {
-//        try {
-            return gotoTable.get(currentState).get(variable);
-//        }catch (NullPointerException dd)
-//        {
-//            dd.printStackTrace();
-//        }
-//        return 0;
+    public int getGotoTable(int currentState, NonTerminal variable) {
+        return gotoTable.get(currentState).get(variable);
     }
-    public Action getActionTable(int currentState ,Token terminal)
-    {
+
+    public Action getActionTable(int currentState, Token terminal) {
         return actionTable.get(currentState).get(terminal);
     }
 
 }
+
